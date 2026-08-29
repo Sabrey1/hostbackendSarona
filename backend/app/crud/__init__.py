37230d007acc8826b2c18643d1e61cfe5dfdac6d
Sabrey1 <limsabriey94@gmail.com>
2026-08-29 16:26:32 +0700

    update

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -2,7 +2,7 @@ from .user.user import *
 from .category.category import *
 
 __all__ = [
-    "create_user", "get_all_users", "get_user", "update_user", "delete_user"
+    "create_user", "get_all_users", "get_user", "update_user", "delete_user",
     "create_category", "get_all_category", "get_category", "update_category", "delete_category"
     
     ]
